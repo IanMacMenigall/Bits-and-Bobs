acdf0f178e232302d13fab6091798b59000328ca
NielsHealey <s3559029@student.rmit.edu.au>
2016-11-01 22:09:13 +1100

    Inital Graphics and Scripts

---
+++ Peter Parkour Pizza/Assets/Menu and UI/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public bool isStart;
+	public bool isQuit;
+
+	void OnMouseUp(){
+		if(isStart)
+		{
+			Application.LoadLevel(1);
+		}
+		if (isQuit)
+		{
+			Application.Quit();
+		}
+	} 
