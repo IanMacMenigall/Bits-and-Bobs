40c0a4425a98a157528b0964427cc4b5f50ae638
LukeChinoBits <s3558273@student.rmit.edu.au>
2016-11-12 15:30:59 +1000

    Camera update

--- Peter Parkour Pizza/Assets/Scripts/CameraController2.cs
+++ Peter Parkour Pizza/Assets/Scripts/CameraController2.cs
@@ -1,35 +1,78 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraController2 : MonoBehaviour {
+public class CameraController2 : MonoBehaviour
+{
 
     public SimplePlatformController target;
+    public float verticalOffset;
+    public float lookAheadDstX;
+    public float lookSmoothTimeX;
+    public float verticalSmoothTime;
     public Vector2 focusAreaSize;
 
     FocusArea focusArea;
 
-    void Start(){
-        focusArea = new FocusArea(target.collider.bounds, focusAreaSize);
+    float currentLookAheadX;
+    float targetLookAheadX;
+    float lookAheadDirX;
+    float smoothLookVelocityX;
+    float smoothVelocityY;
+
+    bool lookAheadStopped;
+
+    void Start()
+    {
+        focusArea = new FocusArea(target.GetComponent<Collider2D>().bounds, focusAreaSize);
     }
 
-    void LateUpdate(){
-        focusArea.Update(target.collider.bounds);
+    void LateUpdate()
+    {
+        focusArea.Update(target.GetComponent<Collider2D>().bounds);
+
+        Vector2 focusPosition = focusArea.centre + Vector2.up * verticalOffset;
+
+        if (focusArea.velocity.x != 0)
+        {
+            lookAheadDirX = Mathf.Sign(focusArea.velocity.x);
+            if (Mathf.Sign(Input.GetAxis("Horizontal")) == Mathf.Sign(focusArea.velocity.x) && Input.GetAxis("Horizontal") != 0)
+            {
+                lookAheadStopped = false;
+                targetLookAheadX = lookAheadDirX * lookAheadDstX;
+            }
+            else {
+                if (!lookAheadStopped)
+                {
+                    lookAheadStopped = true;
+                    targetLookAheadX = currentLookAheadX + (lookAheadDirX * lookAheadDstX - currentLookAheadX) / 4f;
+                }
+            }
+        }
+
+
+        currentLookAheadX = Mathf.SmoothDamp(currentLookAheadX, targetLookAheadX, ref smoothLookVelocityX, lookSmoothTimeX);
+
+        focusPosition.y = Mathf.SmoothDamp(transform.position.y, focusPosition.y, ref smoothVelocityY, verticalSmoothTime);
+        focusPosition += Vector2.right * currentLookAheadX;
+        transform.position = (Vector3)focusPosition + Vector3.forward * -10;
     }
 
     void OnDrawGizmos()
     {
         Gizmos.color = new Color(1, 0, 0, .5f);
-        CalculateRaySpacing();
+        Gizmos.DrawCube(focusArea.centre, focusAreaSize);
     }
 
-    struct FocusArea{
-
+    struct FocusArea
+    {
         public Vector2 centre;
         public Vector2 velocity;
         float left, right;
         float top, bottom;
 
-        public FocusArea(Bounds targetBounds, Vector2 size){
+
+        public FocusArea(Bounds targetBounds, Vector2 size)
+        {
             left = targetBounds.center.x - size.x / 2;
             right = targetBounds.center.x + size.x / 2;
             bottom = targetBounds.min.y;
@@ -37,19 +80,21 @@ public class CameraController2 : MonoBehaviour {
 
             velocity = Vector2.zero;
             centre = new Vector2((left + right) / 2, (top + bottom) / 2);
-
         }
-        public void Update(Bounds targetBounds){
+
+        public void Update(Bounds targetBounds)
+        {
             float shiftX = 0;
-            if (targetBounds.min.x < left) {
+            if (targetBounds.min.x < left)
+            {
                 shiftX = targetBounds.min.x - left;
             }
-            else if (targetBounds.max.x > right){
+            else if (targetBounds.max.x > right)
+            {
                 shiftX = targetBounds.max.x - right;
             }
             left += shiftX;
             right += shiftX;
-
 
             float shiftY = 0;
             if (targetBounds.min.y < bottom)
@@ -58,15 +103,13 @@ public class CameraController2 : MonoBehaviour {
             }
             else if (targetBounds.max.y > top)
             {
-                shiftX = targetBounds.max.y - top;
+                shiftY = targetBounds.max.y - top;
             }
-            left += shiftY;
-            right += shiftY;
-
+            top += shiftY;
+            bottom += shiftY;
             centre = new Vector2((left + right) / 2, (top + bottom) / 2);
             velocity = new Vector2(shiftX, shiftY);
-
         }
-
     }
+
 }
