7c6cb350b328e1de0ed259b8356aceed4d3bedc1
AaronGregoryBITS <s3588007@student.rmit.edu.au>
2016-11-14 01:28:24 +1000

    Player Animation update

--- Peter Parkour Pizza/Assets/Scripts/SimplePlatformController.cs
+++ Peter Parkour Pizza/Assets/Scripts/SimplePlatformController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.ComponentModel;
 
 public class SimplePlatformController : MonoBehaviour {
     [HideInInspector]
@@ -14,7 +15,7 @@ public class SimplePlatformController : MonoBehaviour {
 
     private bool grounded = false;
     private Animator anim;
-    private Rigidbody2D rb2d;
+    public Rigidbody2D rb2d;
 
 	// Use this for initialization
 	void Awake () {
@@ -52,7 +53,6 @@ public class SimplePlatformController : MonoBehaviour {
             rb2d.AddForce(new Vector2(0f, jumpForce));
             jump = false;
         }
-           
     }
 
     void Flip()
---
+++ Peter Parkour Pizza/Assets/Scripts/SpriteRotate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteRotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(Vector3.back, GetComponentInParent<SimplePlatformController>().rb2d.velocity.x);
+	}
+}
