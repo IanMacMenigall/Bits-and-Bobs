162998cede36855997a0b891831fe60184752749
AaronGregoryBITS <s3588007@student.rmit.edu.au>
2016-11-15 05:29:49 +1000

    Difficulty update
    Increased damage of enemies and obstacles.
    Decreased recovery from toppings.
    I found I could just hold right and win pretty much, so I adjusted it.

--- Peter Parkour Pizza/Assets/Scripts/Collectable.cs
+++ Peter Parkour Pizza/Assets/Scripts/Collectable.cs
@@ -18,15 +18,15 @@ public class Collectable : MonoBehaviour {
         {
             if (gameObject.CompareTag("pepperoni"))
             {
-                HealthBar.playerHealth = HealthBar.playerHealth + 20;
+                HealthBar.playerHealth = HealthBar.playerHealth + 10;
             }
             else if (gameObject.CompareTag("tomato"))
             {
-                HealthBar.playerHealth = HealthBar.playerHealth + 25;
+                HealthBar.playerHealth = HealthBar.playerHealth + 12;
             }
             else if (gameObject.CompareTag("mushroom"))
             {
-                HealthBar.playerHealth = HealthBar.playerHealth + 30;
+                HealthBar.playerHealth = HealthBar.playerHealth + 15;
             }
 
             Destroy(gameObject);
